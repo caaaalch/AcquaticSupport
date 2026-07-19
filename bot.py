81a233609a28cd629af045b580a4cdff928907dd
caaaalch <chricalchi09@gmail.com>
2026-07-19 15:03:20 +0200

    D

---
+++ bot.py
@@ -0,0 +1,26 @@
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} è online!")
+
+
+bot.run(TOKEN)
